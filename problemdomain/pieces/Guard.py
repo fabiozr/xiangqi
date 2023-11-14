10090f5a15761017e6ed02a764dbf929bf9ed37a
gustavokf2003 <gustavokf2003@gmail.com>
2023-11-14 18:51:18 -0300

    added getReachablePositions

--- problemdomain/pieces/Guard.py
+++ problemdomain/pieces/Guard.py
@@ -11,4 +11,14 @@ class Guard(Piece):
     def getReachablePositions(
         self, position: "Position", matrix_board: list[list["Position"]]
     ) -> list["Position"]:
-        pass
+        reacheble_positions = []
+        x, y = position.getCoordenates()
+        possible_cords = [(x + dx, y + dy) for dx, dy in [(1, 1), (1, -1), (-1, 1), (-1, -1)]]
+
+        for x, y in possible_cords:
+            condition = 3 <= y <= 5 and (0 <= x <= 2 or 7 <= x <= 9)
+
+            if condition:
+                reacheble_positions.append(matrix_board[x][y])
+
+        return reacheble_positions
